5ba295d0dd60a739995f00424aaa855391b861b2
Thiago 'Jedi' Cerqueira <thiago.cerqueira@agilecontent.com>
2022-05-04 14:08:21 -0300

    Added config

---
+++ index.js
@@ -0,0 +1,30 @@
+﻿const concentricOrder = require("stylelint-config-concentric-order");
+
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    // Add other extends before this line
+    "stylelint-prettier/recommended",
+  ],
+  plugins: [
+    "stylelint-order", //To keep order of properties
+  ],
+  rules: {
+    "declaration-empty-line-before": null,
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"],
+      },
+    ],
+    "order/properties-order": concentricOrder.rules[
+      "order/properties-order"
+    ].map((group) =>
+      group.map((rule) => ({
+        properties: rule.properties,
+        emptyLineBefore: "always",
+        noEmptyLineBetween: true,
+      }))
+    ),
+  },
+};
